b014ae490f913cf5c0f0fc9b0aa78da3cf6e3b17
bleen <bleencn@gmail.com>
2024-01-22 22:55:39 +0800

    style: svg icon viewBox

--- components/HomeSwiper.tsx
+++ components/HomeSwiper.tsx
@@ -441,7 +441,7 @@ function Hour(props: { hour?: number }) {
         xmlns="http://www.w3.org/2000/svg"
         width="1.2em"
         height="1.2em"
-        viewBox="0 0 14 14"
+        viewBox="0 0 15 15"
         fill="none"
         className="inline-block align-sub"
       >
